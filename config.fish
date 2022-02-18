4ea8a1f5e3d8384a5f4a9dd9f3f113a59595c94e
davemo88 <david@kasofsk.xyz>
2022-02-18 16:03:52 -0500

    changes

--- config.fish
+++ config.fish
@@ -1,10 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -U fish_greeting "🍥"
+set -U fish_greeting ""
 fish_vi_key_bindings
 set fish_color_valid_path
 fzf_configure_bindings
 function fish_mode_prompt
   # NOOP - Disable vim mode indicator
 end
+bass source ~/sgxsdk/environment
+alias bat=batcat
